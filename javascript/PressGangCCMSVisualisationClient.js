3070c906c9d68d1ababc03d70c2d326143e7a93c
Matthew Casperson <mcaspers@redhat.com>
2013-11-05 18:02:56 +1000

    Working on adding filter

--- javascript/PressGangCCMSVisualisationClient.js
+++ javascript/PressGangCCMSVisualisationClient.js
@@ -342,7 +342,7 @@ function createParticles() {
                         values_color[values_color.length - 1].setRGB(2, 4, 8);
                     }
                 } else {
-                    values_color[values_color.length - 1].setRGB(0.25, 0.25, 0.25);
+                    values_color[values_color.length - 1].setRGB(0.01, 0.01, 0.01);
                 }
 
                 // add it to the geometry
